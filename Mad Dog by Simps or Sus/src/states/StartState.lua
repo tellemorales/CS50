1f368bd7be606df145fca5fab97e2419ce742634
hcacasjr23 <61836013+hcacasjr23@users.noreply.github.com>
2021-02-20 18:33:11 +0800

    Update from keyboard to mouse function

--- Mad Dog by Simps or Sus/src/states/StartState.lua
+++ Mad Dog by Simps or Sus/src/states/StartState.lua
@@ -1,72 +1,9 @@
 
 StartState = Class{__includes = BaseState}
 -- whether we're highlighting "Start" or "High Scores"
-local highlighted = 0
 
-
-function StartState:update(dt)
-
-    if love.keyboard.wasPressed(key) then  
-        gSounds['select']:play()
-    end
-
-    -- toggle highlighted option if we press an arrow key up or down
-    if love.keyboard.wasPressed('up') then
-            highlighted = highlighted - 1
-        if highlighted == 0 then
-            highlighted = 4 
-        end
-
-        
-    elseif love.keyboard.wasPressed('down') then
-        highlighted = highlighted + 1
-        if highlighted == 2 then
-            highlighted = 3
-        end
-        if highlighted == 5 then
-            highlighted = 1
-        end
-
-
-    elseif love.keyboard.wasPressed('right') then
-        highlighted = highlighted + 1
-        if highlighted == 5 then
-            highlighted = 1
-        end
-
-    elseif love.keyboard.wasPressed('left') then
-        highlighted = highlighted - 1
-        if highlighted == 0 then
-            highlighted = 4
-        end
-       
-    end
-
-    -- we no longer have this globally, so include here
-    if love.keyboard.wasPressed('escape') then
-        love.event.quit()
-    end
-end
-
-
-function StartState:render()
-
-    -- get x coordinate of each image
-    local x = {
-        ['play'] = (VIRTUAL_WIDTH / 2) + 150,
-        ['option'] = (VIRTUAL_WIDTH / 2) + 420,
-        ['highscore'] = (VIRTUAL_WIDTH / 2) + 150,
-        ['quit'] = (VIRTUAL_WIDTH / 2) + 150
-    }
-    -- get y coordinate of each image
-    local y = {
-        ['play'] = (VIRTUAL_HEIGHT / 2) - 130,
-        ['option'] = (VIRTUAL_HEIGHT / 2) - 130,
-        ['highscore'] = (VIRTUAL_HEIGHT / 2) - 25,
-        ['quit'] = (VIRTUAL_HEIGHT / 2) + 80
-    }
-    
-    local button_quad = {
+function StartState:init()
+    self.button = {
         ['play'] = love.graphics.newQuad(0, 220 , 285, 110, gTextures['main_men']:getDimensions()),
         ['selected_play'] = love.graphics.newQuad(0, 330 , 285, 110, gTextures['main_men']:getDimensions()),
         ['option'] = love.graphics.newQuad(769, 0, 470, 110, gTextures['main_men']:getDimensions()),
@@ -77,22 +14,64 @@ function StartState:render()
         ['selected_highscore'] = love.graphics.newQuad(0, 110 , 769, 110, gTextures['main_men']:getDimensions()),
     }
 
-    -- get width
-    local w = {
-        ['play'] = 285,
-        ['option'] = 470,
-        ['highscore'] = 769,
-        ['quit'] = 769,
+    self.x = {
+        ['play'] = (VIRTUAL_WIDTH / 2) + 140,
+        ['option'] = (VIRTUAL_WIDTH / 2) + 430,
+        ['highscore'] = (VIRTUAL_WIDTH / 2) + 140,
+        ['quit'] = (VIRTUAL_WIDTH / 2) + 140
     }
-    
-    -- get height
-    local h = {
-        ['play'] = 110,
-        ['option'] = 110,
-        ['highscore'] = 110,
-        ['quit'] = 111
+
+    self.y = {
+        ['play'] = (VIRTUAL_HEIGHT / 2) - 130,
+        ['option'] = (VIRTUAL_HEIGHT / 2) - 130,
+        ['highscore'] = (VIRTUAL_HEIGHT / 2) - 15,
+        ['quit'] = (VIRTUAL_HEIGHT / 2) + 100
     }
-    
+
+
+    self.width = {
+        ['play'] = 257,
+        ['option'] = 371,
+        ['highscore'] = 661,
+        ['quit'] = 661,
+    }
+
+    self.height = {
+        ['play'] = 93,
+        ['option'] = 93,
+        ['highscore'] = 93,
+        ['quit'] = 94
+    }
+
+
+end
+
+
+
+function StartState:update(dt)
+
+ if isPressed(key) then  
+        gSounds['select']:play()
+    end
+
+    if love.mouseIn(self.x['play'], self.y['play'], self.width['play'], self.height['play']) and isPressed(1) then
+        gStateMachine:change('play')
+        
+    elseif love.mouseIn(self.x['option'], self.y['option'], self.width['option'], self.height['option']) and isPressed(1) then
+        gStateMachine:change('option')
+
+    elseif love.mouseIn(self.x['quit'], self.y['quit'], self.width['quit'], self.height['quit']) and isPressed(1) then
+        love.event.quit()
+    end
+
+    -- we no longer have this globally, so include here
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+
+function StartState:render()
     
     -- title
     love.graphics.draw(gTextures['logo'],
@@ -117,44 +96,43 @@ function StartState:render()
         -- rotation
          0,
         -- size
-         .830, .800,
+         .850, .800,
         -- filter 
          love.graphics.setDefaultFilter("nearest","nearest")
         )
-    -- play button
-    love.graphics.draw(gTextures['main_men'], button_quad['play'], x['play'], y['play'], 0, .900, .850)
+    
+   -- play button
+    love.graphics.draw(gTextures['main_men'], self.button['play'], self.x['play'], self.y['play'], 0, .900, .850)
     
     -- option button
-    love.graphics.draw(gTextures['main_men'], button_quad['option'], x['option'], y['option'], 0,.790, .850)
+    love.graphics.draw(gTextures['main_men'], self.button['option'], self.x['option'], self.y['option'], 0,.790, .850)
 
     -- highscore button
-    love.graphics.draw(gTextures['main_men'], button_quad['highscore'], x['highscore'], y['highscore'], 0, .835, .850)
+    love.graphics.draw(gTextures['main_men'], self.button['highscore'], self.x['highscore'], self.y['highscore'], 0, .860, .850)
 
     -- quit
-    love.graphics.draw(gTextures['main_men'], button_quad['quit'], x['quit'], y['quit'], 0, .835, .850)
-    
+    love.graphics.draw(gTextures['main_men'], self.button['quit'], self.x['quit'], self.y['quit'], 0, .860, .850)
  
            
     
-    -- if we're highlighting 1, change image 
-    if highlighted == 1 or love.mouseIn(x['play'], y['play'], w['play'], h['play']) then
-        love.graphics.draw(gTextures['main_men'], button_quad['selected_play'], x['play'], y['play'], 0, .900, .850)
+    -- if mouse hovers, change image 
+
+    if love.mouseIn(self.x['play'], self.y['play'], self.width['play'], self.height['play']) then
+        love.graphics.draw(gTextures['main_men'], self.button['selected_play'], self.x['play'], self.y['play'], 0, .900, .850)
     
     end
 
-
-    -- change image if we're highlighting 2
-    if highlighted == 2 or love.mouseIn(x['option'], y['option'], w['option'], h['option']) then
-        love.graphics.draw(gTextures['main_men'], button_quad['selected_option'], x['option'], y['option'], 0, .790, .850)   
+    if love.mouseIn(self.x['option'], self.y['option'], self.width['option'], self.height['option']) then
+        love.graphics.draw(gTextures['main_men'], self.button['selected_option'], self.x['option'], self.y['option'], 0, .790, .850)   
     end
 
 
-    if highlighted == 3 or love.mouseIn(x['highscore'], y['highscore'], w['highscore'], h['highscore']) then
-        love.graphics.draw(gTextures['main_men'], button_quad['selected_highscore'], x['highscore'], y['highscore'], 0, .835, .850)
+    if love.mouseIn(self.x['highscore'], self.y['highscore'], self.width['highscore'], self.height['highscore']) then
+        love.graphics.draw(gTextures['main_men'], self.button['selected_highscore'], self.x['highscore'], self.y['highscore'], 0, .860, .850)
     end
 
 
-    if highlighted == 4 or love.mouseIn(x['quit'], y['quit'], w['quit'], h['quit']) then
-        love.graphics.draw(gTextures['main_men'], button_quad['selected_quit'], x['quit'], y['quit'], 0, .835, .850) 
+    if love.mouseIn(self.x['quit'], self.y['quit'], self.width['quit'], self.height['quit']) then
+        love.graphics.draw(gTextures['main_men'], self.button['selected_quit'], self.x['quit'], self.y['quit'], 0, .860, .850) 
     end
 end
