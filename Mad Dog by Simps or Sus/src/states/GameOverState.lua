080014da4c2da30f419f94b93a7f5208e93c736e
hcacasjr23 <hdcjr00@gmail.com>
2021-02-24 21:44:20 +0800

    Added GameOverState

---
+++ Mad Dog by Simps or Sus/src/states/GameOverState.lua
@@ -0,0 +1,32 @@
+GameOverState = Class{__includes = BaseState}
+
+function GameOverState:init()
+
+end
+
+function GameOverState:enter(params)
+    self.score = params.score 
+end
+
+function GameOverState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('start')
+    end
+end
+
+function GameOverState:render()
+    
+    love.graphics.setFont(gFonts['large'])
+
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 64 , 64, 128, 136, 4)
+
+    love.graphics.draw(gTextures['background'], backgroundX, 0)
+
+    love.graphics.setColor(99, 155, 255, 255)
+    love.graphics.printf('GAME OVER', VIRTUAL_WIDTH / 2 - 64, 64, 128, 'center')
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.printf('Your Score: ' .. tostring(self.score), VIRTUAL_WIDTH / 2 - 64, 140, 128, 'center')
+    love.graphics.printf('Press Enter', VIRTUAL_WIDTH / 2 - 64, 180, 128, 'center')
+
+end
